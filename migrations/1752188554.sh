3695126e66b66f950a705a1326d2683990efadbf
Dustin Romey <dustinromey@gmail.com>
2025-07-13 12:51:09 -0400

    make Brave default browser

--- migrations/1752188554.sh
+++ migrations/1752188554.sh
@@ -1,7 +1,13 @@
-echo "Update chromium.desktop to ensure we are always using wayland"
+echo "Update chromium.desktop and brave.desktop to ensure we are always using wayland"
 if [[ ! -f ~/.local/share/applications/chromium.desktop ]]; then
   cp ~/.local/share/omarchy/applications/chromium.desktop ~/.local/share/applications/
   xdg-settings set default-web-browser chromium.desktop
   xdg-mime default chromium.desktop x-scheme-handler/http
   xdg-mime default chromium.desktop x-scheme-handler/https
+fi
+if [[ ! -f ~/.local/share/applications/brave.desktop ]]; then
+  cp ~/.local/share/omarchy/applications/brave.desktop ~/.local/share/applications/
+  xdg-settings set default-web-browser brave.desktop
+  xdg-mime default brave.desktop x-scheme-handler/http
+  xdg-mime default brave.desktop x-scheme-handler/https
 fi
