c1163cc12fad5382050e9a00df1f23198f5cceed
Dustin Romey <dustinromey@gmail.com>
2025-07-13 12:07:37 -0400

    add ghostty

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -3,4 +3,5 @@ yay -S --noconfirm --needed \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate bash-completion \
-  alacritty
+  alacritty \
+  ghostty
